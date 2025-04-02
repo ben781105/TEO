75d62cff098a8eee39b1b31392686e1b9038b7e9
Ssewabuga Benard <159682984+ben781105@users.noreply.github.com>
2025-04-02 14:21:42 +0300

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -13,3 +13,11 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+import sys
+from django.urls import get_resolver
+
+if 'runserver' in sys.argv or 'gunicorn' in sys.argv:
+    print("Registered URLs in Django:")
+    for url in get_resolver().url_patterns:
+        print(url)
