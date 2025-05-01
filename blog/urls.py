e281d1fe8ea5c42c33dc06272266e2673ff66106
ssewabuga <benardleno78@gmail.com>
2025-05-01 16:10:12 +0300

    post

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import postView, content,postsbytags,comments
 
 
-urlpatterns =[
-path('post/',postView, name='post'),
+urlpatterns = [
+path('post',postView, name='post'),
 path('content/<slug:slug>/',content,name='content'),
 path('postsbytags/tag/<str:tag>/',postsbytags,name='postsbytags'),
 path('comments/',comments,name='comments'),
